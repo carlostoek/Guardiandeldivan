458a6f508f5c244e63f3817d41c5150e94c50bff
carlostoek <carlostoek@gmail.com>
2025-06-14 10:03:39 -0600

    Merge pr-34 and add broadcast command

--- handlers/admin/__init__.py
+++ handlers/admin/__init__.py
@@ -1,4 +1,5 @@
 from .token import router as token_router
 from .users import router as users_router
+from .broadcast import router as broadcast_router
 
-__all__ = ["token_router", "users_router"]
+__all__ = ["token_router", "users_router", "broadcast_router"]
